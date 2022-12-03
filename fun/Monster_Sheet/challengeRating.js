65106095ded692efacbe2375780310c862d587c1
Brian Immel <jawa9000@users.noreply.github.com>
2022-12-03 07:17:42 -0800

    init commit

---
+++ fun/Monster_Sheet/challengeRating.js
@@ -0,0 +1,299 @@
+var challengeRating = {
+    "zero": {
+        "cr": "0",
+        "profBonus": 2,
+        "ac": 12,
+        "hp": "1-6",
+        "attackBonus": 2,
+        "saveDC": 12,
+        "xp": 10
+    },
+    "eighth": {
+        "cr": "1/8",
+        "profBonus": 2,
+        "ac": 13,
+        "hp": "7-35",
+        "attackBonus": 3,
+        "saveDC": 13,
+        "xp": 25
+    },
+    "fourth": {
+        "cr": "1/4",
+        "profBonus": 2,
+        "ac": 13,
+        "hp": "36-49",
+        "attackBonus": 3,
+        "saveDC": 13,
+        "xp": 50
+    },
+    "half": {
+        "cr": "1/2",
+        "profBonus": 2,
+        "ac": 13,
+        "hp": "50-70",
+        "attackBonus": 3,
+        "saveDC": 13,
+        "xp": 100
+    },
+    "one": {
+        "cr": "1",
+        "profBonus": 2,
+        "ac": 13,
+        "hp": "71-85",
+        "attackBonus": 3,
+        "saveDC": 13,
+        "xp": 200
+    },
+    "two": {
+        "cr": "2",
+        "profBonus": 2,
+        "ac": 13,
+        "hp": "86-100",
+        "attackBonus": 3,
+        "saveDC": 13,
+        "xp": 450
+    },
+    "three": {
+        "cr": "3",
+        "profBonus": 2,
+        "ac": 13,
+        "hp": "101-115",
+        "attackBonus": 4,
+        "saveDC": 13,
+        "xp": 700
+    },
+    "four": {
+        "cr": "4",
+        "profBonus": 2,
+        "ac": 14,
+        "hp": "116-130",
+        "attackBonus": 5,
+        "saveDC": 14,
+        "xp": 1100
+    },
+    "five": {
+        "cr": "5",
+        "profBonus": 3,
+        "ac": 15,
+        "hp": "131-145",
+        "attackBonus": 6,
+        "saveDC": 15,
+        "xp": 1800
+    },
+    "six": {
+        "cr": "6",
+        "profBonus": 3,
+        "ac": 15,
+        "hp": "131-145",
+        "attackBonus": 6,
+        "saveDC": 15,
+        "xp": 2300
+    },
+    "seven": {
+        "cr": "7",
+        "profBonus": 3,
+        "ac": 15,
+        "hp": "161-171",
+        "attackBonus": 6,
+        "saveDC": 15,
+        "xp": 2900
+    },
+    // "zero": {
+    //     "cr": "0",
+    //     "profBonus": 0,
+    //     "ac": 0,
+    //     "hp": "",
+    //     "attackBonus": 0,
+    //     "saveDC": 0,
+    //     "xp": 0
+    // },
+    // "zero": {
+    //     "cr": "0",
+    //     "profBonus": 0,
+    //     "ac": 0,
+    //     "hp": "",
+    //     "attackBonus": 0,
+    //     "saveDC": 0,
+    //     "xp": 0
+    // },
+    // "zero": {
+    //     "cr": "0",
+    //     "profBonus": 0,
+    //     "ac": 0,
+    //     "hp": "",
+    //     "attackBonus": 0,
+    //     "saveDC": 0,
+    //     "xp": 0
+    // },
+    // "zero": {
+    //     "cr": "0",
+    //     "profBonus": 0,
+    //     "ac": 0,
+    //     "hp": "",
+    //     "attackBonus": 0,
+    //     "saveDC": 0,
+    //     "xp": 0
+    // },
+    // "zero": {
+    //     "cr": "0",
+    //     "profBonus": 0,
+    //     "ac": 0,
+    //     "hp": "",
+    //     "attackBonus": 0,
+    //     "saveDC": 0,
+    //     "xp": 0
+    // },
+    // "zero": {
+    //     "cr": "0",
+    //     "profBonus": 0,
+    //     "ac": 0,
+    //     "hp": "",
+    //     "attackBonus": 0,
+    //     "saveDC": 0,
+    //     "xp": 0
+    // },
+    // "zero": {
+    //     "cr": "0",
+    //     "profBonus": 0,
+    //     "ac": 0,
+    //     "hp": "",
+    //     "attackBonus": 0,
+    //     "saveDC": 0,
+    //     "xp": 0
+    // },
+    // "zero": {
+    //     "cr": "0",
+    //     "profBonus": 0,
+    //     "ac": 0,
+    //     "hp": "",
+    //     "attackBonus": 0,
+    //     "saveDC": 0,
+    //     "xp": 0
+    // },
+    // "zero": {
+    //     "cr": "0",
+    //     "profBonus": 0,
+    //     "ac": 0,
+    //     "hp": "",
+    //     "attackBonus": 0,
+    //     "saveDC": 0,
+    //     "xp": 0
+    // },
+    // "zero": {
+    //     "cr": "0",
+    //     "profBonus": 0,
+    //     "ac": 0,
+    //     "hp": "",
+    //     "attackBonus": 0,
+    //     "saveDC": 0,
+    //     "xp": 0
+    // },
+    // "zero": {
+    //     "cr": "0",
+    //     "profBonus": 0,
+    //     "ac": 0,
+    //     "hp": "",
+    //     "attackBonus": 0,
+    //     "saveDC": 0,
+    //     "xp": 0
+    // },
+    // "zero": {
+    //     "cr": "0",
+    //     "profBonus": 0,
+    //     "ac": 0,
+    //     "hp": "",
+    //     "attackBonus": 0,
+    //     "saveDC": 0,
+    //     "xp": 0
+    // },
+    // "zero": {
+    //     "cr": "0",
+    //     "profBonus": 0,
+    //     "ac": 0,
+    //     "hp": "",
+    //     "attackBonus": 0,
+    //     "saveDC": 0,
+    //     "xp": 0
+    // },
+    // "zero": {
+    //     "cr": "0",
+    //     "profBonus": 0,
+    //     "ac": 0,
+    //     "hp": "",
+    //     "attackBonus": 0,
+    //     "saveDC": 0,
+    //     "xp": 0
+    // },
+    // "zero": {
+    //     "cr": "0",
+    //     "profBonus": 0,
+    //     "ac": 0,
+    //     "hp": "",
+    //     "attackBonus": 0,
+    //     "saveDC": 0,
+    //     "xp": 0
+    // },
+    // "zero": {
+    //     "cr": "0",
+    //     "profBonus": 0,
+    //     "ac": 0,
+    //     "hp": "",
+    //     "attackBonus": 0,
+    //     "saveDC": 0,
+    //     "xp": 0
+    // },
+    // "zero": {
+    //     "cr": "0",
+    //     "profBonus": 0,
+    //     "ac": 0,
+    //     "hp": "",
+    //     "attackBonus": 0,
+    //     "saveDC": 0,
+    //     "xp": 0
+    // },
+    // "zero": {
+    //     "cr": "0",
+    //     "profBonus": 0,
+    //     "ac": 0,
+    //     "hp": "",
+    //     "attackBonus": 0,
+    //     "saveDC": 0,
+    //     "xp": 0
+    // },
+    // "zero": {
+    //     "cr": "0",
+    //     "profBonus": 0,
+    //     "ac": 0,
+    //     "hp": "",
+    //     "attackBonus": 0,
+    //     "saveDC": 0,
+    //     "xp": 0
+    // },
+    // "zero": {
+    //     "cr": "0",
+    //     "profBonus": 0,
+    //     "ac": 0,
+    //     "hp": "",
+    //     "attackBonus": 0,
+    //     "saveDC": 0,
+    //     "xp": 0
+    // },
+    // "zero": {
+    //     "cr": "0",
+    //     "profBonus": 0,
+    //     "ac": 0,
+    //     "hp": "",
+    //     "attackBonus": 0,
+    //     "saveDC": 0,
+    //     "xp": 0
+    // },
+    // "zero": {
+    //     "cr": "0",
+    //     "profBonus": 0,
+    //     "ac": 0,
+    //     "hp": "",
+    //     "attackBonus": 0,
+    //     "saveDC": 0,
+    //     "xp": 0
+    // },
+}
