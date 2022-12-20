124e2f6b38de3ef21c19468ba1920940bae4663c
Brian Immel <jawa9000@users.noreply.github.com>
2022-12-20 10:53:02 -0800

    Update skills.js
    Added a few new skills.

--- js/skills.js
+++ js/skills.js
@@ -19,7 +19,7 @@ let skills = {
         "Jira": ['Project management', 'Workflows', 'Instructor']
     },
     "Documentation types": ['API documentation', 'Application notes', 'Help manuals', 'Integration documentation', 'Online help systems', 'Reference manuals', 'Release notes', 'Training Guides', 'Tutorials', 'User guides'],
-    "Communication": ['Flowdock', 'Google Hangouts', 'HipChat', 'Join.me', 'MS Teams', 'Slack', 'Skype', 'Webex'],
+    "Communication": ['Workplace', 'Flowdock', 'Google Hangouts', 'HipChat', 'Join.me', 'MS Teams', 'Slack', 'Skype', 'Webex'],
     "General skills": ['3D modeling', '3D animation', 'API', 'Computer animation', 'Content management systems', 'Desktop recording', 'Digital Photography', 'Documentation', 'Front-end web developer', 'Front-end web development', 'Live classroom training', 'Photography', 'Proofreading', 'Regular expressions', 'Remote training', 'Responsive', 'Responsive Design', 'REST API', 'ReSTful', 'Software training', 'SDK', 'Teaching', 'Technical writing', 'Training', 'Verbal communication', 'Video editing', 'Video production', 'Video recording', 'Web Design', 'Web Development', 'Writing'],
     "Software": {
         "Adobe Suite": ['After Effects', 'FrameMaker', 'Illustrator', 'Lightroom', 'Photoshop', 'Premiere'],
@@ -28,6 +28,7 @@ let skills = {
         "IDEs": ['Appcelerator Studio', 'Aptana', 'Atom', 'Dreamweaver', 'Eclipse', 'VS Code'],
         "Wiki": ['MediaWiki','Confluence'],
         "Misc graphics": ['Camtasia', 'Gimp', 'Lucidchart', 'Maya', 'Record My Desktop', 'Visio'],
-        "Microsoft": ['Excel', 'Outlook', 'PowerPoint', 'Sharepoint', 'Word']
+        "Microsoft": ['Excel', 'Outlook', 'PowerPoint', 'Sharepoint', 'Word'],
+        "Misc": ['ADP', 'Workday']
     }
 }
