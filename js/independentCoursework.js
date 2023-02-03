1afaaec2496cce750715f41a61f9fff7478a1bcb
Brian Immel <jawa9000@users.noreply.github.com>
2023-02-03 08:58:46 -0800

    Update independentCoursework.js

--- js/independentCoursework.js
+++ js/independentCoursework.js
@@ -1,14 +1,14 @@
 let independentCoursework = {
+    // "": {
+    //     "name": ""  
+    // },
     "Introduction to JavaScript": {
         "name": "Introduction to JavaScript (P131)"  
     },
     "Intro to CSS": {
-        "name": "Intro to CSS (N/A)"  
+        "name": "Intro to CSS"  
     },
     "CSS": {
-        "name": "CSS: The Cool Stuff (and so much more) (N/A)"  
+        "name": "CSS: The Cool Stuff (and so much more)"  
     }
-    // "": {
-    //     "name": ""  
-    // },
 }
