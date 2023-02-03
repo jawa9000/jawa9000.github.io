f56c86458c80c8a02df229e8eec27b7f57ef8e20
Brian Immel <jawa9000@users.noreply.github.com>
2023-02-03 08:58:42 -0800

    Update certifications.js
    Added new certs, re-orderded content, and added some documentation.

--- js/certifications.js
+++ js/certifications.js
@@ -1,4 +1,153 @@
+/* 
+    Usage: Put newer certificaitons on the top
+    Links are optional if the cert isn't available
+*/
+
 let certifications = {
+    // "": {
+    //     "link": "",
+    //     "title": "",
+    //     "date": "",
+    //     "meta": [""]
+    // },
+    "Confidence: How to Overcome Self-Doubt, Insecurity, and Fears": {
+        "link": "https://www.linkedin.com/learning/certificates/8d7dc7abe2ef763a52b90650e0ddee3ed929837c5bee17485bb00cf4de6f681a",
+        "title": "Confidence: How to Overcome Self-Doubt, Insecurity, and Fears",
+        "date": "January 2023",
+        "meta": ["Anxiety", "Self-confidence"]
+    },
+    "Overcome Overthinking": {
+        "link": "https://www.linkedin.com/learning/certificates/b84aa7ba3510afad82e6ed1917b7c5f3c8a0b8a41413e8c53d9d74647a3a5148",
+        "title": "Overcome Overthinking",
+        "date": "January 2023",
+        "meta": ["Intrapersonal skills"]
+    },
+    "Avoiding New Manager Mistakes": {
+        "link": "https://www.linkedin.com/learning/certificates/420b26103cd5101eece6ee36f22a204a393d64fb8fada19f9e6cdfa778c10c95?u=67698794",
+        "title": "Avoiding New Manager Mistakes",
+        "date": "January 2023",
+        "meta": ["Management"]
+    },
+    "Managing Teams": {
+        "link": "https://www.linkedin.com/learning/certificates/e2cc49f3687b03e5e41f74dc0d3b5c71047d1dab74bc8e501ab2d4effb2e7e4a?u=67698794",
+        "title": "Managing Teams",
+        "date": "January 2023",
+        "meta": ["Team management"]
+    },
+    "The Three Pillars of Effective Communication": {
+        "link": "https://www.linkedin.com/learning/certificates/25bc132799c256939b74d30dc24699740f0c157ae997fec63e97d7a18cfa0a2c?u=67698794",
+        "title": "The Three Pillars of Effective Communication",
+        "date": "January 2023",
+        "meta": ["Interpersonal communication","Building trust"]
+    },
+    "Working with High-Conflict People as a Manager": {
+        "link": "https://www.linkedin.com/learning/certificates/e5071b8facf65d3479237465fe68d903fe9b93fc627f31e62fa988eaf16a02fa?u=67698794",
+        "title": "Working with High-Conflict People as a Manager",
+        "date": "January 2023",
+        "meta": ["Interpersonal skills", "Conflict management"]
+    },
+    "Change Management Foundations": {
+        "link": "",
+        "title": "Change Management Foundations",
+        "date": "March 2022",
+        "meta": ["Change management", "Management"]
+    },
+    "Creating Psychological Safety for Diverse Teams": {
+        "link": "",
+        "title": "Creating Psychological Safety for Diverse Teams",
+        "date": "March 2022",
+        "meta": ["Team management"]
+    },
+    "Creating a Culture of Change": {
+        "link": "",
+        "title": "Creating a Culture of Change",
+        "date": "March 2022",
+        "meta": ["Culture", "Management"]
+    },
+    "Introduction to Web APIs": {
+        "link": "",
+        "title": "Introduction to Web APIs",
+        "date": "March 2022",
+        "meta": ["API"]
+    },
+    "Leading Culture Change in Your Team": {
+        "link": "",
+        "title": "Leading Culture Change in Your Team",
+        "date": "March 2022",
+        "meta": ["Culture", "Management"]
+    },
+    "Leading When You're Not in Charge": {
+        "link": "",
+        "title": "Leading When You're Not in Charge",
+        "date": "March 2022",
+        "meta": ["Team leadership"]
+    },
+    "Learning How to Increase Learner Engagement": {
+        "link": "",
+        "title": "Learning How to Increase Learner Engagement",
+        "date": "March 2022",
+        "meta": ["Learning", "Engaging"]
+    },
+    "Project Management Simplified": {
+        "link": "https://www.linkedin.com/learning/certificates/7cde078f6b8ee70a9871c4432d7fbb9ad914796ee8ba532adffdfd53b4b43613?u=67698794",
+        "title": "Project Management Simplified",
+        "date": "April 2022",
+        "meta": ["Project management"]
+    },
+    "Teaching Technques: Blended Learning": {
+        "link": "https://www.linkedin.com/learning/certificates/2a3791fea72784bbc429e8fef0f350615ea6f24c229a23ddd3233ba31c572625?u=67698794",
+        "title": "Teaching Technques: Blended Learning",
+        "date": "April 2022",
+        "meta": ["Blended learning", "Teaching"]
+    },
+    "Coping Strategies in Difficult Times": {
+        "link": "https://www.linkedin.com/learning/certificates/d3c8c8c7db0f63509a090bcd06907b6e35cb7a8dbd00b89c2f4e26996da05101",
+        "title": "Coping Strategies in Difficult Times",
+        "date": "May 2022",
+        "meta": ["Productivity", "Coping"]
+    },
+    "Using Authenticity to Build Productive Relationships": {
+        "link": "https://www.linkedin.com/learning/certificates/241b73eb99a92a4b77d001954644560fbd9438b038defb3a0f538336ea6bae13?u=67698794",
+        "title": "Using Authenticity to Build Productive Relationships",
+        "date": "May 2022",
+        "meta": ["Relationship building", "Productivity"]
+    },
+    "Content Creation: Strategy and Tools": {
+        "link": "https://www.linkedin.com/learning/certificates/1cadcf1ce05463bbb10f91fb02e047462225701ff5d73df56d304fd71c49e55c?u=67698794",
+        "title": "Content Creation: Strategy and Tools",
+        "date": "June 2022",
+        "meta": ["Content strategy", "Social media content creation"]
+    },
+    "Critical Thinking for More Effective Communication": {
+        "link": "https://www.linkedin.com/learning/certificates/df6df9283098f1ffaadff78ceddea52dc2dcc14d4712c8cde360f81a44e5334f?u=67698794",
+        "title": "Critical Thinking for More Effective Communication",
+        "date": "June 2022",
+        "meta": ["Critical thinking"]
+    },
+    "How to Create a Career You Love": {
+        "link": "https://www.linkedin.com/learning/certificates/62c36f8f2fa64997c374e84d38aee804176cbe25a22deba2305804b1397506c2?u=67698794",
+        "title": "How to Create a Career You Love",
+        "date": "June 2022",
+        "meta": ["Career opportunities"]
+    },
+    "Overcoming Cognitive Bias": {
+        "link": "https://www.linkedin.com/learning/certificates/59bd92050ff77db83e78da02690c5271a14e63f0c101ea18cc9da4e297fa1c12?u=67698794",
+        "title": "Overcoming Cognitive Bias",
+        "date": "June 2022",
+        "meta": ["Critical thinking"]
+    },
+    "Project Management Foundations: Requirements": {
+        "link": "https://www.linkedin.com/learning/certificates/19becabb0849d257fa508d92ccd0cbe80540b658ec166e842eefe1e38a146e7c?u=67698794",
+        "title": "Project Management Foundations: Requirements",
+        "date": "June 2022",
+        "meta": ["Requirements management"]
+    },
+    "How to Slash Anxiety and Keep Positivity Flowing": {
+        "link": "https://www.linkedin.com/learning/certificates/1ecc25be83960cf9872a24aea02c925a2ae5d18577f5dec5e757632b9ec8385e",
+        "title": "How to Slash Anxiety and Keep Positivity Flowing",
+        "date": "November 2022",
+        "meta": ["Productivity", "Anxiety"]
+    },
     "Learn Python, it's CAKE (Beginners)": {
         "link": "https://www.udemy.com/certificate/UC-950b11d2-b7a3-4f4a-8576-866390e5d183/",
         "title": "Learn Python, it's CAKE (Beginners)",
@@ -11,23 +160,17 @@ let certifications = {
         "date": "February 2021",
         "meta": ["Nodejs"]
     },
+    "How to Develop your Career Plan": {
+        "link": "",
+        "title": "How to Develop your Career Plan",
+        "date": "September 2021",
+        "meta": ["Career opportunities"]
+    },
     "Learning MySQL5 - An Easy Way To Master MySQL": {
         "link": "https://www.udemy.com/certificate/UC-d63a0bcd-7a11-472f-bf44-b3ce1d69788e/",
         "title": "Learning MySQL5 - An Easy Way To Master MySQL",
         "date": "December 2020",
         "meta": ["MySQL"]
-    },
-    "How to use Javascript Objects JSON Ajax explained": {
-        "link": "https://www.udemy.com/certificate/UC-CUUX79CX/",
-        "title": "How to use Javascript Objects JSON Ajax explained",
-        "date": "June 2016",
-        "meta": ["Javascript", "JSON"]
-    },
-    "Git Started with GitHub": {
-        "link": "https://www.udemy.com/certificate/UC-953ERMRI/",
-        "title": "Git Started with GitHub",
-        "date": "June 2016",
-        "meta": ["Git", "GitHub"]
     },
     "Learn Web Scraping with Node.js": {
         "link": "https://www.udemy.com/certificate/UC-DBVENW6O/",
@@ -47,11 +190,23 @@ let certifications = {
         "date": "January 2016",
         "meta": ["Git"]
     },
-    "Basics of Bash Scripting": {
-        "link": "https://www.udemy.com/certificate/UC-44NH4YZ3/",
-        "title": "Basics of Bash Scripting",
-        "date": "December 2015",
-        "meta": ["Bash"]
+    "How to use Javascript Objects JSON Ajax explained": {
+        "link": "https://www.udemy.com/certificate/UC-CUUX79CX/",
+        "title": "How to use Javascript Objects JSON Ajax explained",
+        "date": "June 2016",
+        "meta": ["Javascript", "JSON"]
+    },
+    "Git Started with GitHub": {
+        "link": "https://www.udemy.com/certificate/UC-953ERMRI/",
+        "title": "Git Started with GitHub",
+        "date": "June 2016",
+        "meta": ["Git", "GitHub"]
+    },
+    "Learn API Technical Writing 2: JSON and XML for Writers": {
+        "link": "https://www.udemy.com/certificate/UC-J3757RN0/",
+        "title": "Learn API Technical Writing 2: JSON and XML for Writers",
+        "date": "September 2015",
+        "meta": ["JSON", "XML", "API", "technical writing"]
     },
     "Learn API Technical Writing 2: REST for Writers": {
         "link": "https://www.udemy.com/certificate/UC-ZVBZ7XUZ/",
@@ -65,35 +220,47 @@ let certifications = {
         "date": "October 2015",
         "meta": ["Javascript"]
     },
-    "Learn API Technical Writing 2: JSON and XML for Writers": {
-        "link": "https://www.udemy.com/certificate/UC-J3757RN0/",
-        "title": "Learn API Technical Writing 2: JSON and XML for Writers",
-        "date": "September 2015",
-        "meta": ["JSON", "XML", "API", "technical writing"]
+    "Basics of Bash Scripting": {
+        "link": "https://www.udemy.com/certificate/UC-44NH4YZ3/",
+        "title": "Basics of Bash Scripting",
+        "date": "December 2015",
+        "meta": ["Bash"]
+    },    
+    "Become a Certified Web Developer": {
+        "link": "https://www.udemy.com/certificate/UC-K127ZL78/",
+        "title": "Become a Certified Web Developer",
+        "date": "February 2014",
+        "meta": ["HTML", "CSS", "Javascript"]
     },
-    "Step by Step Sass": {
-        "link": "https://www.udemy.com/certificate/UC-I567YNTK/",
-        "title": "Step by Step Sass",
-        "date": "December 2014",
-        "meta": ["Sass", "CSS"]
+    "HTML Tutorial HTML CSS for Beginners": {
+        "link": "https://www.udemy.com/certificate/UC-LD2I98CV/",
+        "title": "HTML Tutorial: HTML & CSS for Beginners",
+        "date": "February 2014",
+        "meta": ["HTML", "CSS"]
     },
-    "1 Hour Javascript": {
-        "link": "https://www.udemy.com/certificate/UC-UFUF1YW6/",
-        "title": "1 Hour Javascript",
-        "date": "December 2014",
-        "meta": ["Javascript"]
+    "PHP Tutorial PHP Login Script Creation": {
+        "link": "https://www.udemy.com/certificate/UC-RS75QWIJ/",
+        "title": "PHP Tutorial: PHP Login Script Creation",
+        "date": "March 2014",
+        "meta": ["PHP"]
     },
-    "Learn Nodejs by Example": {
-        "link": "https://www.udemy.com/certificate/UC-CMXBRM4E/",
-        "title": "Learn Nodejs by Example",
-        "date": "November 2014",
-        "meta": ["Nodejs"]
+    "Java The Beginners Series": {
+        "link": "https://www.udemy.com/certificate/UC-J59DPC5Q/",
+        "title": "Java - The Beginners Series",
+        "date": "April 2014",
+        "meta": ["Java"]
     },
-    "Learn HTML5 in 1 hour": {
-        "link": "https://www.udemy.com/certificate/UC-FLK37F2D/",
-        "title": "Learn HTML5 in 1 hour",
-        "date": "November 2014",
-        "meta": ["HTML5"]
+    "Learn Responsive Web Development from Scratch": {
+        "link": "https://www.udemy.com/certificate/UC-SVKZPBVI/",
+        "title": "Learn Responsive Web Development from Scratch",
+        "date": "May 2014",
+        "meta": ["Bootstrap", "Responsive"]
+    },
+    "Learn to Build Websites using Twitter Bootstrap": {
+        "link": "https://www.udemy.com/certificate/UC-YJSQYAPU/",
+        "title": "Learn to Build Websites using Twitter Bootstrap",
+        "date": "June 2014",
+        "meta": ["Bootstrap", "Responsive", "Javascript", "CSS"]
     },
     "GitHub Fundamentals": {
         "link": "https://www.udemy.com/certificate/UC-LCHYWC9Z/",
@@ -107,40 +274,34 @@ let certifications = {
         "date": "September 2014",
         "meta": ["regular expressions", "regex"]
     },
-    "Learn to Build Websites using Twitter Bootstrap": {
-        "link": "https://www.udemy.com/certificate/UC-YJSQYAPU/",
-        "title": "Learn to Build Websites using Twitter Bootstrap",
-        "date": "June 2014",
-        "meta": ["Bootstrap", "Responsive", "Javascript", "CSS"]
+    "Learn Nodejs by Example": {
+        "link": "https://www.udemy.com/certificate/UC-CMXBRM4E/",
+        "title": "Learn Nodejs by Example",
+        "date": "November 2014",
+        "meta": ["Nodejs"]
     },
-    "Learn Responsive Web Development from Scratch": {
-        "link": "https://www.udemy.com/certificate/UC-SVKZPBVI/",
-        "title": "Learn Responsive Web Development from Scratch",
-        "date": "May 2014",
-        "meta": ["Bootstrap", "Responsive"]
+    "Learn HTML5 in 1 hour": {
+        "link": "https://www.udemy.com/certificate/UC-FLK37F2D/",
+        "title": "Learn HTML5 in 1 hour",
+        "date": "November 2014",
+        "meta": ["HTML5"]
     },
-    "Java The Beginners Series": {
-        "link": "https://www.udemy.com/certificate/UC-J59DPC5Q/",
-        "title": "Java - The Beginners Series",
-        "date": "April 2014",
-        "meta": ["Java"]
+    "Step by Step Sass": {
+        "link": "https://www.udemy.com/certificate/UC-I567YNTK/",
+        "title": "Step by Step Sass",
+        "date": "December 2014",
+        "meta": ["Sass", "CSS"]
     },
-    "PHP Tutorial PHP Login Script Creation": {
-        "link": "https://www.udemy.com/certificate/UC-RS75QWIJ/",
-        "title": "PHP Tutorial: PHP Login Script Creation",
-        "date": "March 2014",
-        "meta": ["PHP"]
+    "1 Hour Javascript": {
+        "link": "https://www.udemy.com/certificate/UC-UFUF1YW6/",
+        "title": "1 Hour Javascript",
+        "date": "December 2014",
+        "meta": ["Javascript"]
     },
-    "Become a Certified Web Developer": {
-        "link": "https://www.udemy.com/certificate/UC-K127ZL78/",
-        "title": "Become a Certified Web Developer",
-        "date": "February 2014",
-        "meta": ["HTML", "CSS", "Javascript"]
-    },
-    "HTML Tutorial HTML CSS for Beginners": {
-        "link": "https://www.udemy.com/certificate/UC-LD2I98CV/",
-        "title": "HTML Tutorial: HTML & CSS for Beginners",
-        "date": "February 2014",
-        "meta": ["HTML", "CSS"]
+    "Sass Next Steps": {
+        "link": "https://www.udemy.com/certificate/UC-I567YNTK/",
+        "title": "Sass Next Steps",
+        "date": "December 2014",
+        "meta": ["CSS", "Sass"]
     }
 }
