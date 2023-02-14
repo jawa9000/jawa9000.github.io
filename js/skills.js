88b1fa2aa3086700d4c96538c330ae85b09d9330
Brian Immel <jawa9000@users.noreply.github.com>
2023-02-14 10:14:56 -0800

    Update skills.js
    Added new skills and reordered a few.

--- js/skills.js
+++ js/skills.js
@@ -7,7 +7,7 @@ let skills = {
         "Indexing": ['Apache Solr'],
         "Version Control": ['CVS', 'Git', 'Github'],
         "Web services": ['WAMP', 'XAMPP'],
-        "General": ['Database design','Front-end web development','Regular expressions','Responsive design','Web design','Web development']
+        "General": ['Database design', 'Front-end web development', 'Regular expressions', 'Responsive design', 'Web design', 'Web development']
     },
     "Operating systems": {
         "Windows": ['7.x', '8.x', '10.x'],
@@ -19,19 +19,19 @@ let skills = {
         "Confluence": ['Administration', 'Front-end development', 'Theme design', 'Plugin management', 'User Macro development', 'Instructor'],
         "Jira": ['Project management', 'Workflows', 'Instructor']
     },
-    "Technical writing skills": ['API', 'Confluence API', 'Content feedback integration', 'Content management systems','Content research','Documentation','Documentation analysis','Documentation at scale','Documentation automation','Documentation communication','Documentation publication automation','Documentation site design','information architecture','Live classroom training','program/project reports','Project manager','Proofreading','Remote training','REST API','ReSTful','SDK','Style guides','Technical writing','Writing','Archives wiki','Documentation procedures','Documentation tracking','Documentation maintenance','Documentation reporting','Site documentation','IT documentation','Wiki documentation'],
-    "Documentation types": ['Animated images','API documentation','Application note','Architecture guide','Diagrams','Glossary','Help manual','Illustration','Integration documentation','Landing page','Long-form video','Online help system','Overview','Reference','Reference manual','Release note','SDK reference','Short-form video','Static images','Training Guide','Tutorials','User guides'],
-    
-    "Photography and video": ['Desktop recording','Digital Photography','Photography','Video editing','Video production','Video recording'],
-    
-    "Communication": ['Flowdock', 'Google Chat', 'HipChat', 'Join.me', 'MS Teams', 'Slack', 'Skype', 'Webex','Workplace', 'Zoom'],
-    "General skills": ['3D animation','3D modeling','Analytics','Analytics dashboard designer','Asynchronous communication','Collaboration','Computer animation','Cross-functional team communication','Cross-functional team relationship development','Educator','Fluency in English','Interviewer','Mentor','Presenter','Program leader/manager','Social event organizer','Software training','Synchronous communication','Teaching','Trainer','Verbal communication','Written communication'],
+    "Technical writing skills": ['API', 'Archives wiki', 'Automation in application development', 'Confluence API', 'Content analysis', 'Content Curation', 'Content development', 'Content feedback integration', 'Content management systems', 'Content planning', 'Content research', 'Documentation', 'Documentation analysis', 'Documentation at scale', 'Documentation automation', 'Documentation communication', 'Documentation maintenance', 'Documentation procedures', 'Documentation publication automation', 'Documentation reporting', 'Documentation site design', 'Documentation tracking', 'information architecture', 'IT documentation', 'Live classroom training', 'program/project reports', 'Project management scheduling', 'Project manager', 'Project planning management', 'Proofreading', 'Remote training', 'REST API', 'ReSTful', 'SDK', 'Site documentation', 'Style guides', 'Technical writing', 'Wiki documentation', 'Writing'],
+    "Documentation types": ['Animated images', 'API documentation', 'Application note', 'Architecture guide', 'Diagrams', 'Glossary', 'Help manual', 'Illustration', 'Integration documentation', 'Landing page', 'Long-form video', 'Online help system', 'Overview', 'Reference', 'Reference manual', 'Release note', 'SDK reference', 'Short-form video', 'Static images', 'Training Guide', 'Tutorials', 'User guides'],
+
+    "Photography and video": ['Desktop recording', 'Digital Photography', 'Photography', 'Video editing', 'Video production', 'Video recording'],
+
+    "Communication": ['Flowdock', 'Google Chat', 'HipChat', 'Join.me', 'MS Teams', 'Slack', 'Skype', 'Webex', 'Workplace', 'Zoom'],
+    "General skills": ['3D animation', '3D modeling', 'Analytics', 'Analytics dashboard designer', 'Asynchronous communication', 'Collaboration', 'Computer animation', 'Cross-functional team communication', 'Cross-functional team relationship development', 'Educator', 'Fluency in English', 'Interviewer', 'Mentor', 'Presenter', 'Program leader/manager', 'Social event organizer', 'Software training', 'Synchronous communication', 'Teaching', 'Trainer', 'Verbal communication', 'Written communication'],
     "Software": {
         "Adobe Suite": ['After Effects', 'FrameMaker', 'Illustrator', 'Lightroom', 'Photoshop', 'Premiere'],
         "FTP": ['FileZilla'],
         "Google": ['Calendar', 'Docs', 'Drawings', 'Drive', 'Forms', 'Gmail', 'Sheets', 'Sites', 'Slides'],
         "IDEs": ['Appcelerator Studio', 'Aptana', 'Atom', 'Dreamweaver', 'Eclipse', 'VS Code'],
-        "Wiki": ['Confluence','MediaWiki'],
+        "Wiki": ['Confluence', 'MediaWiki'],
         "Misc graphics": ['Camtasia', 'Gimp', 'Lucidchart', 'Maya', 'Record My Desktop', 'SnagIt', 'Visio'],
         "Microsoft": ['Excel', 'Outlook', 'PowerPoint', 'Sharepoint', 'Word'],
         "Misc": ['ADP', 'Workday']
