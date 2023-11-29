e09b42f566289837b8d02fa3231a907c389e5e49
Brian Immel <jawa9000@users.noreply.github.com>
2023-11-28 16:45:53 -0800

    Create mine_data.js
    Moved data to its own file.

---
+++ fun/Running_Fantasy_Business/js/mine_data.js
@@ -0,0 +1,671 @@
+let minedOutput = {
+    granite: {
+      name: "Granite",
+      gpOutput: [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6],
+      pricePerPound: 0.1,
+    },
+    basalt: {
+      name: "Basalt",
+      gpOutput: [1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12],
+      pricePerPound: 0.2,
+    },
+    marble: {
+      name: "Marble",
+      gpOutput: [2, 4, 8, 16, 18, 20, 22, 24, 26, 28, 30, 32],
+      pricePerPound: 0.4,
+    },
+    coal: {
+      name: "Coal",
+      gpOutput: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+      pricePerPound: 0.2,
+    },
+    petroleum: {
+      name: "Petroleum",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    agate: {
+      name: "agate",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    bloodstone: {
+      name: "bloodstone",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    canelian: {
+      name: "canelian",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    citrine: {
+      name: "citrine",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    jasper: {
+      name: "jasper",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    moonstone: {
+      name: "moonstone",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    onyx: {
+      name: "onyx",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    tourmaline: {
+      name: "tourmaline",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    zircon: {
+      name: "zircon",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    fluorite: {
+      name: "fluorite",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    obsidian: {
+      name: "obsidian",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    quartz: {
+      name: "quartz",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    amethyst: {
+      name: "amethyst",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    "lapis lazuli": {
+      name: "lapis lazuli",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    hematite: {
+      name: "hematite",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    rhodochrosite: {
+      name: "rhodochrosite",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    carnelian: {
+      name: "carnelian",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    chrysopidate: {
+      name: "chrysopidate",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    alexandrite: {
+      name: "alexandrite",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    jade: {
+      name: "jade",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    azurite: {
+      name: "azurite",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    malachite: {
+      name: "malachite",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    turquoise: {
+      name: "turquoise",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    amber: {
+      name: "amber",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    jet: {
+      name: "jet",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    "blue calcite": {
+      name: "blue calcite",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    aquamarine: {
+      name: "aquamarine",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    blueSpinel: {
+      name: "blue spinel",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    peridot: {
+      name: "peridot",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    topaz: {
+      name: "topaz",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    garnet: {
+      name: "garnet",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    jacinth: {
+      name: "jacinth",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    opal: {
+      name: "opal",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    redSpinel: {
+      name: "red spinel",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    sapphire: {
+      name: "sapphire",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    emerald: {
+      name: "emerald",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    ruby: {
+      name: "ruby",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    diamond: {
+      name: "diamond",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    radioactive: {
+      name: "radioactive",
+      gpOutput: [4.5, 6.5, 8.5, 10, 12.5, 14.5, 16, 18, 20, 22, 25, 31],
+      pricePerPound: 500,
+    },
+    lead: {
+      name: "Lead",
+      gpOutput: [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6],
+      pricePerPound: 0.2,
+    },
+    zinc: {
+      name: "Zinc",
+      gpOutput: [1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12],
+      pricePerPound: 0.2,
+    },
+    iron: {
+      name: "Iron",
+      gpOutput: [2, 3.5, 5, 6.5, 8, 9.5, 11, 12, 14, 15.5, 17, 18.5],
+      pricePerPound: 0.4,
+    },
+    tin: {
+      name: "Tin",
+      gpOutput: [3.5, 5.5, 7.5, 9.5, 11, 13.5, 15, 18, 20, 21.5, 23.5, 26],
+      pricePerPound: 0.3,
+    },
+    copper: {
+      name: "Copper",
+      gpOutput: [5, 7.5, 10, 12.5, 15, 17.5, 20, 22.5, 25, 27.5, 30, 32.5],
+      pricePerPound: 0.5,
+    },
+    silver: {
+      name: "Silver",
+      gpOutput: [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65],
+      pricePerPound: 5,
+    },
+    gold: {
+      name: "Gold",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+      pricePerPound: 50,
+    },
+    platinum: {
+      name: "Platinum",
+      gpOutput: [40, 65, 79, 112, 136, 160, 185, 210, 232, 280, 305, 330],
+      pricePerPound: 500,
+    },
+    mithril: {
+      name: "Mithril",
+      gpOutput: [75, 105, 140, 170, 205, 240, 275, 310, 345, 385, 415, 455],
+      pricePerPound: 2500,
+    },
+    adamantine: {
+      name: "Adamantine",
+      gpOutput: [100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650],
+      pricePerPound: 5000,
+    },
+  };
+
+  let miningProducts = {
+    stones: {
+      name: "stones",
+      first: 0,
+      second: 64,
+    },
+    metals: {
+      name: "metals",
+      first: 65,
+      second: 82,
+    },
+    exotics: {
+      name: "exotics",
+      first: 83,
+      second: 92,
+    },
+    gemstones: {
+      name: "gemstones",
+      first: 93,
+      second: 99,
+    },
+  };
+
+  let metals = {
+    lead: {
+      name: "lead",
+      first: 0,
+      second: 4,
+      gpOutput: [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6],
+    },
+    zinc: {
+      name: "zinc",
+      first: 5,
+      second: 9,
+      gpOutput: [1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12],
+    },
+    iron: {
+      name: "iron",
+      first: 10,
+      second: 59,
+      byproduct: "ironTable",
+      gpOutput: [2, 3.5, 5, 6.5, 8, 9.5, 11, 12, 14, 15.5, 17, 18.5],
+    },
+    tin: {
+      name: "tin",
+      first: 60,
+      second: 69,
+      gpOutput: [3.5, 5.5, 7.5, 9.5, 11, 13.5, 15, 18, 20, 21.5, 23.5, 26],
+    },
+    copper: {
+      name: "copper",
+      first: 70,
+      second: 84,
+      byproduct: "copperTable",
+      gpOutput: [5, 7.5, 10, 12.5, 15, 17.5, 20, 22.5, 25, 27.5, 30, 32.5],
+    },
+    silver: {
+      name: "silver",
+      first: 85,
+      second: 89,
+      gpOutput: [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65],
+    },
+    gold: {
+      name: "gold",
+      first: 90,
+      second: 94,
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+    },
+    platinum: {
+      name: "platinum",
+      first: 95,
+      second: 96,
+      gpOutput: [80, 130, 177, 225, 275, 320, 370, 420, 465, 560, 610, 660],
+    },
+    mithril: {
+      name: "mithril",
+      first: 97,
+      second: 98,
+      gpOutput: [150, 210, 280, 340, 410, 480, 550, 620, 690, 770, 830, 910],
+    },
+    adamantine: {
+      name: "adamantine",
+      first: 99,
+      second: 100,
+      gpOutput: [
+        200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300,
+      ],
+    },
+  };
+
+  let stones = {
+    granite: {
+      name: "granite",
+      first: 0,
+      second: 59,
+      gpOutput: [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6],
+    },
+    basalt: {
+      name: "basalt",
+      first: 60,
+      second: 89,
+      gpOutput: [1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12],
+    },
+    marble: {
+      name: "marble",
+      first: 90,
+      second: 99,
+      gpOutput: [2, 4, 8, 16, 18, 20, 22, 24, 26, 28, 30, 32],
+    },
+  };
+
+  let exotics = {
+    coal: {
+      name: "coal",
+      first: 0,
+      second: 59,
+      byproduct: "amber-jet",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+    },
+    petroleum: {
+      name: "petroleum",
+      first: 60,
+      second: 89,
+      byproduct: "blue calcite",
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+    },
+  //   radioactive: {
+  //     name: "radioactive",
+  //     first: 90,
+  //     second: 99,
+  //     gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+  //   }
+  };
+
+  let gemstones = {
+    aquamarine: {
+      name: "aquamarine",
+      first: 0,
+      second: 14,
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+    },
+    blueSpinel: {
+      name: "blueSpinel",
+      first: 15,
+      second: 29,
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+    },
+    peridot: {
+      name: "peridot",
+      first: 30,
+      second: 44,
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+    },
+    topaz: {
+      name: "topaz",
+      first: 45,
+      second: 59,
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+    },
+    garnet: {
+      name: "garnet",
+      first: 60,
+      second: 67,
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+    },
+    jacinth: {
+      name: "jacinth",
+      first: 68,
+      second: 75,
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+    },
+    opal: {
+      name: "opal",
+      first: 76,
+      second: 83,
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+    },
+    redSpinel: {
+      name: "red spinel",
+      first: 84,
+      second: 90,
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+    },
+    sapphire: {
+      name: "sapphire",
+      first: 91,
+      second: 93,
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+    },
+    emerald: {
+      name: "emerald",
+      first: 94,
+      second: 95,
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+    },
+    ruby: {
+      name: "ruby",
+      first: 96,
+      second: 97,
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+    },
+    diamond: {
+      name: "diamond",
+      first: 98,
+      second: 99,
+      gpOutput: [17.5, 22.5, 38.5, 50, 67, 72, 87, 100, 110, 117, 130],
+    },
+  };
+
+  let graniteByProducts = {
+    nothing: {
+      name: "nothing",
+      first: 0,
+      second: 49,
+    },
+    agate: {
+      name: "agate",
+      first: 50,
+      second: 69,
+    },
+    bloodstone: {
+      name: "bloodstone",
+      first: 70,
+      second: 71,
+    },
+    canelian: {
+      name: "canelian",
+      first: 72,
+      second: 74,
+    },
+    citrine: {
+      name: "citrine",
+      first: 75,
+      second: 77,
+    },
+    jasper: {
+      name: "jasper",
+      first: 78,
+      second: 80,
+    },
+    moonstone: {
+      name: "moonstone",
+      first: 81,
+      second: 83,
+    },
+    onyx: {
+      name: "onyx",
+      first: 84,
+      second: 86,
+    },
+    tourmaline: {
+      name: "tourmaline",
+      first: 87,
+      second: 89,
+    },
+    zircon: {
+      name: "zircon",
+      first: 90,
+      second: 92,
+    },
+    fluorite: {
+      name: "fluorite",
+      first: 93,
+      second: 94,
+    },
+    rollTwice: {
+      name: "rollTwice",
+      first: 95,
+      second: 99,
+    },
+  };
+
+  let basaltByProdcts = {
+    nothing: {
+      name: "nothing",
+      first: 0,
+      second: 49,
+    },
+    agate: {
+      name: "agate",
+      first: 50,
+      second: 61,
+    },
+    obsidian: {
+      name: "obsidian",
+      first: 62,
+      second: 74,
+    },
+    quartz: {
+      name: "quartz",
+      first: 75,
+      second: 84,
+    },
+    amethyst: {
+      name: "amethyst",
+      first: 85,
+      second: 89,
+    },
+    fluorite: {
+      name: "fluorite",
+      first: 90,
+      second: 94,
+    },
+    rollTwice: {
+      name: "rollTwice",
+      first: 95,
+      second: 99,
+    },
+  };
+
+  let marbelByProducts = {
+    nothing: {
+      name: "nothing",
+      first: 0,
+      second: 49,
+    },
+    "lapis lazuli": {
+      name: "lapis lazuli",
+      first: 50,
+      second: 89,
+    },
+    rollOnceGranite: {
+      name: "rollOnceGranite",
+      first: 90,
+      second: 94,
+    },
+    rollTwiceGranite: {
+      name: "rollTwiceGranite",
+      first: 95,
+      second: 99,
+    },
+  };
+
+  let ironByProducts = {
+    nothing: {
+      name: "nothing",
+      first: 0,
+      second: 49,
+    },
+    hematite: {
+      name: "hematite",
+      first: 50,
+      second: 70,
+    },
+    rhodochrosite: {
+      name: "rhodochrosite",
+      first: 71,
+      second: 86,
+    },
+    bloodstone: {
+      name: "bloodstone",
+      first: 87,
+      second: 89,
+    },
+    carnelian: {
+      name: "carnelian",
+      first: 90,
+      second: 92,
+    },
+    chrysopidate: {
+      name: "chrysopidate",
+      first: 93,
+      second: 95,
+    },
+    alexandrite: {
+      name: "alexandrite",
+      first: 96,
+      second: 97,
+    },
+    jade: {
+      name: "jade",
+      first: 98,
+      second: 99,
+    },
+  };
+
+  let copperByProducts = {
+    nothing: {
+      name: "nothing",
+      first: 0,
+      second: 49,
+    },
+    azurite: {
+      name: "azurite",
+      first: 50,
+      second: 65,
+    },
+    malachite: {
+      name: "malachite",
+      first: 66,
+      second: 81,
+    },
+    turquoise: {
+      name: "turquoise",
+      first: 82,
+      second: 00,
+    },
+  };
