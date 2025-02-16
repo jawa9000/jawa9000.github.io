18a030f5194cab447095779b73e06ee289b0ca3b
Brian Immel <jawa9000@users.noreply.github.com>
2025-02-15 17:07:07 -0800

    Create encounter_multipler.js

---
+++ fun/Encounter_Builder/encounter_multipler.js
@@ -0,0 +1,26 @@
+const encounterMultiplier = {
+    "Level 1": {
+        numberOfMonsters: [1],
+        multiplier: 1
+    },
+    "Level 2": {
+        numberOfMonsters: [2],
+        multiplier: 1.5
+    },
+    "Level 3": {
+        numberOfMonsters: [3, 6],
+        multiplier: 2
+    },
+    "Level 4": {
+        numberOfMonsters: [7, 10],
+        multiplier: 2.5
+    },
+    "Level 5": {
+        numberOfMonsters: [11, 14],
+        multiplier: 3
+    },
+    "Level 6": {
+        numberOfMonsters: [15, 100],
+        multiplier: 4
+    }
+}
