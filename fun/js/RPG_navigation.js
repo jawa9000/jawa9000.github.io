ccc2669dc26955b23675375b4d5238b4a18abc9e
Brian Immel <jawa9000@users.noreply.github.com>
2025-04-20 13:44:31 -0700

    Create RPG_navigation.js

---
+++ fun/js/RPG_navigation.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    const urls = [ // update this list with new RGP tools are added.
+      "../Apothecary/index.html, Apothecary",
+      "../Critical_Hit_Fumble/index.html, Criticals",
+      "../Dragon_Generator/index.html, Dragons",
+      "../Encounter_Builder/index.html, Encounters",
+      "../Fantasy_Character_Generator/index.html, Char Gen",
+      "../Monster_Sheet/index.html, Monster Sheet",
+      "../Random_Weather/index.html, Weather",
+      "../Running_Fantasy_Business/index.html, Business",
+      "../Running_Fantasy_Business/mines.html, Mines",
+      "../Running_Fantasy_Business/ulrye.html, Ulrye",
+      "../Spell_list/index.html, Spells",
+      "../Treasure_Generator/index.html, Treasure"
+    ];
+  
+    let bannerHTML = '<div class="banner_nav">';
+
+    $.each(urls, function(index, urlData) {
+      const parts = urlData.split(',');
+      const url = parts[0].trim();
+      const name = parts[1].trim();
+      bannerHTML += `<a href="${url}" class="rpg_link">${name}</a>`;
+    });
+  
+    bannerHTML += '</div>';
+  
+    $('body').prepend(bannerHTML);
+  });
